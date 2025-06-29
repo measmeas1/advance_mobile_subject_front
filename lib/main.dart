a4396d03906f968d06ff9744c24fb7d95a4bbb51
Tou Sophanith <sophaneth53@gmail.com>
2025-06-29 16:04:15 +0700

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/Screens/Auth/login_screen.dart';
-import 'package:frontend/Screens/account_screen.dart';
-import 'package:frontend/Screens/cart_screen.dart';
-import 'package:frontend/Screens/home_screen.dart';
-import 'package:frontend/Screens/search_screen.dart';
+import 'package:frontend/Screens/bottom_nav.dart';
 import 'package:frontend/providers/cart_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +11,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -21,43 +20,12 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Shopping App',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: LoginScreen(),
+        home: BottomNav(),
         debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
 
-class MainNavigation extends StatefulWidget {
-  const MainNavigation({super.key});
 
-  @override
-  _MainNavigationState createState() => _MainNavigationState();
-}
 
-class _MainNavigationState extends State<MainNavigation> {
-  int _index = 0;
-  final _screens = [
-    HomeScreen(),
-    SearchScreen(),
-    CartScreen(),
-    AccountScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (value) => setState(() => _index = value),
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account"),
-        ],
-      ),
-    );
-  }
-}
